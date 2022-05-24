a853b77321303759eb69e25e978b52d63cf6283d
Joshua Saylor <joshuasaylor01@gmail.com>
2022-05-24 14:58:23 -0400

    end of day started JS

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-index.js
+
+const key = "ab685c35"
+const searchBtn = document.getElementById("searchBtn")
+
+
+
+    let searchInput = document.getElementById("search").value
+    
+    searchBtn.addEventListener("click", function(){
+        fetch(`http://www.omdbapi.com/?apikey=${key}&s=${searchInput}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    })
+    
